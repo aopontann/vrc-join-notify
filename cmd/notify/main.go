dc3977980cf2b157e9282c73f12c16f46bfb6a0b
aopontann <aopontan0416@gmail.com>
2025-09-02 18:45:24 +0900

    余分なハンドラ関数を削除

--- cmd/notify/main.go
+++ cmd/notify/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 
 	c "github.com/aopontann/vrc-join-notify"
@@ -40,7 +39,7 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/", c.NotifyHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -52,85 +51,4 @@ func main() {
 		slog.Error("something went terribly wrong: " + err.Error())
 		return
 	}
-}
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	db, err := c.NewDB()
-	if err != nil {
-		ErrorHandler(w, r, err, http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
-
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
-	if err != nil {
-		ErrorHandler(w, r, err, http.StatusInternalServerError)
-		return
-	}
-
-	userInfos, err := db.GetAllUserInfo()
-	if err != nil {
-		ErrorHandler(w, r, err, http.StatusInternalServerError)
-		return
-	}
-
-	vrc := c.NewVRC()
-	for discordID, userInfo := range userInfos {
-		// トークンがまだ有効か確認
-		ok, err := vrc.VerifyAuthToken(userInfo.Token)
-		if err != nil {
-			ErrorHandler(w, r, err, http.StatusInternalServerError)
-			continue
-		}
-		if !ok && !userInfo.Notificationed{
-			slog.Warn("Auth token is invalid", "discordID", discordID)
-			// Discordへの通知
-			if _, err := discord.ChannelMessageSend(userInfo.ChannelID, "再ログインしてください。"); err != nil {
-				ErrorHandler(w, r, err, http.StatusInternalServerError)
-				continue
-			}
-			// 次回実行時に通知しないようにするための処理
-			if err := db.ChangeNotificationed(discordID, true); err != nil {
-				ErrorHandler(w, r, err, http.StatusInternalServerError)
-			}
-			continue
-		}
-
-		// ターゲットユーザの情報を取得
-		tu, err := vrc.GetUserInfo(userInfo.TargetVRCUserID, userInfo.Token, userInfo.TwoFactorAuthToken)
-		if err != nil {
-			slog.Error("Failed to get target user info", "error", err)
-			continue
-		}
-		if tu.State == "online" && tu.Status == "join me" && !userInfo.Notificationed {
-			// Discordへの通知
-			if _, err := discord.ChannelMessageSend(userInfo.ChannelID, tu.DisplayName+" さんがオンラインになりました。"); err != nil {
-				ErrorHandler(w, r, err, http.StatusInternalServerError)
-				continue
-			}
-
-			// 次回実行時に通知しないようにするための処理
-			err := db.ChangeNotificationed(discordID, true)
-			if err != nil {
-				ErrorHandler(w, r, err, http.StatusInternalServerError)
-				continue
-			}
-
-		}
-		// オフラインになった場合、通知フラグをFALSEに戻す
-		if tu.State == "online" && userInfo.Notificationed {
-			err := db.ChangeNotificationed(discordID, false)
-			if err != nil {
-				ErrorHandler(w, r, err, http.StatusInternalServerError)
-				continue
-			}
-		}
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
-
-func ErrorHandler(w http.ResponseWriter, r *http.Request, err error, status int) {
-	slog.Error(err.Error())
-	http.Error(w, err.Error(), status)
 }
